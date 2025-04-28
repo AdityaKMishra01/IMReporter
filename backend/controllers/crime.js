bb5d8343f4ec5b84ac13e2c54b39c5f11c1ad427
adityakmishra <adityamishra1872@gmail.com>
2025-04-28 10:17:59 +0530

    folder Added

--- backend/controllers/crime.js
+++ backend/controllers/crime.js
@@ -1,5 +1,14 @@
 const crime = require("../models/crimeUpload");
+const multer = require('multer');
 const cloudinary = require("../config/cloudnary");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // unique name
+  }
+});
+const upload = multer({ storage: storage });
 
 const registerCrime = async (req, res) => {
   try {
@@ -9,20 +18,24 @@ const registerCrime = async (req, res) => {
       crimecategory,
       crimedesc,
       crimelocation,
-      images,
     } = req.body;
 
-    if (!userid || !crimelocation || !crimecatogrie) {
+    if (!userid || !crimelocation || !crimecategory) {
       return res
         .status(401)
         .json({ status: "failed", message: "Enter required details!" });
     }
 
+    if(userid.length !== 24){
+      return res.status(400).json({message:"Invalid User ID"})
+    }
+
     //   uploading image to cloudinary
     const ImagesURL = [];
-    for (const image of images) {
-      const result = await cloudinary.uploader.upload(image, {
+    for (const image of req.files) {
+      const result = await cloudinary.uploader.upload(image.path, {
         folder: "crime_posts",
+        resource_type: "auto",
       });
       ImagesURL.push(result.secure_url);
     }
@@ -45,4 +58,4 @@ const registerCrime = async (req, res) => {
 };
 
 
-module.exports = {registerCrime}
+module.exports = {registerCrime,upload}
